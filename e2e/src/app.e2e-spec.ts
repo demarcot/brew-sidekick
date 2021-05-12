0c24838e38de81b81351ae3b12a9148c0ddd42e2
Thomas DeMarco <tom_demarco1013@yahoo.com>
2021-05-12 19:35:07 -0400

    Tom - Updated project name.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('brew-calc app is running!');
+    expect(page.getTitleText()).toEqual('brew-sidekick app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'brew-calc'`, () => {
+  it(`should have as title 'brew-sidekick'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('brew-calc');
+    expect(app.title).toEqual('brew-sidekick');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('brew-calc app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('brew-sidekick app is running!');
   });
 });
