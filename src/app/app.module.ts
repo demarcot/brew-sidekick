640851379cb66d048da608ec03d558db348f9622
Thomas DeMarco <tom_demarco1013@yahoo.com>
2021-05-15 14:12:07 -0400

    Tom - Added volume calculation based on weight and specific gravity. Updated style a bit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -18,7 +26,10 @@ import { ReactiveFormsModule } from '@angular/forms';
     BrowserAnimationsModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    MatCardModule
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -17,13 +17,13 @@ export class LandingComponent implements OnInit {
     // - Sparge Vol
     // - Est Pre Boil Grav
     // - Pre Boil Vol
-    public targetMashTemp: FormControl = new FormControl(152);
-    public grainWeight: FormControl = new FormControl(10);
-    public absRate: FormControl = new FormControl(0.125);
-    public evapRate: FormControl = new FormControl(0.5);
-    public mashTunLoss: FormControl = new FormControl(.21);
-    public targetOg: FormControl = new FormControl(1.060);
-    public batchVol: FormControl = new FormControl(5);
+    public targetMashTemp = new FormControl(152);
+    public grainWeight = new FormControl(10);
+    public absRate = new FormControl(0.125);
+    public evapRate = new FormControl(0.5);
+    public mashTunLoss = new FormControl(.21);
+    public targetOg = new FormControl(1.060);
+    public batchVol = new FormControl(5);
 
     
     public mashThickness: number; // .3125gal/lb
@@ -39,22 +39,30 @@ export class LandingComponent implements OnInit {
     // Brew Corrections
     // - Raise Pre Boil Grav
     // - Lower Pre Boil Grav
-    public collectedPreboilVol: FormControl = new FormControl(6);
-    public measuredGrav: FormControl = new FormControl(1.059);
-    public targetGrav: FormControl = new FormControl(1.060);
+    public collectedPreboilVol = new FormControl(6);
+    public measuredGrav = new FormControl(1.059);
+    public targetGrav = new FormControl(1.060);
     // Weight of addition = (Volume of wort * (Target gravity – Measured gravity)) / Extract potential points of addition
     // Light DME Potential Extract: 1.045
     public dmeAdd: number;
     // Increase (or decrease) in boiling time in minutes = (Pre-boil volume * (Target pre-boil gravity points – Actual pre-boil gravity points) * 60) / (Target pre-boil gravity points * Boiling losses per hour)
     public extraBoilMins: number;
 
+    // Volume Measuring
+    // Assuming holding pot and standing on scale
+    // 8.32lbs/gal = 1kg/L
+    // vol = (currWeight-emptyWeight)/(8.32*specificGrav)
+    public currWeight = new FormControl();
+    public emptyWeight = new FormControl();
+    public specificGrav = new FormControl();
+    public currVol: number;
 
     // Post Brew Calculations
     // - Efficiency
     // - ABV
-    public potentialGrav: FormControl = new FormControl(1.084);
-    public measuredOG: FormControl = new FormControl(1.059);
-    public measuredFG: FormControl = new FormControl(1.060);
+    public potentialGrav = new FormControl(1.084);
+    public measuredOG = new FormControl(1.059);
+    public measuredFG = new FormControl(1.060);
     public efficiency: number;
     public abv: number;
 
@@ -72,6 +80,10 @@ export class LandingComponent implements OnInit {
         collectedPreboilVol: this.collectedPreboilVol,
         measuredGrav: this.measuredGrav,
         targetGrav: this.targetGrav,
+
+        currWeight: this.currWeight,
+        emptyWeight: this.emptyWeight,
+        specificGrav: this.specificGrav,
 
         potentialGrav: this.potentialGrav,
         measuredOG: this.measuredOG,
@@ -105,6 +117,8 @@ export class LandingComponent implements OnInit {
 
         this.dmeAdd = 5;
         this.extraBoilMins = 5;
+
+        this.currVol = (this.currWeight.value - this.emptyWeight.value)/(8.32 * this.specificGrav.value);
 
         this.efficiency = (1.0-this.measuredOG.value)/(1.0-this.potentialGrav.value);
         this.abv = 0.1;
